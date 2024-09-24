5bdab4e84653fec460b3b424f5eceaeadb1f085a
Julien Faure <julien.faure@iaca-electronique.com>
2024-09-24 13:54:31 +0200

    Add getVitalsById method to VitalsController

--- src/controllers/VitalsController.ts
+++ src/controllers/VitalsController.ts
@@ -2,4 +2,5 @@ import {Request, Response} from 'express';
 
 export interface VitalsController {
     getVitals(req: Request, res: Response) : void;
+    getVitalsById(req: Request, res: Response) : void;
 }
